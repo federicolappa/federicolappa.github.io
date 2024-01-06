6c9dc1737a0a86d9ba990688c4e8253fa42c6a4c
Federico Lappa <federicolappa@gmail.com>
2024-01-06 20:00:12 +0100

    update

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,4 +3,31 @@ document.addEventListener('DOMContentLoaded', () => {
     document.querySelectorAll('a[href^="#"]').forEach(anchor => {
         anchor.addEventListener('click', function (e) {
             e.preventDefault();
-            document.querySelector
+            document.querySelector(this.getAttribute('href')).scrollIntoView({
+                behavior: 'smooth'
+            });
+        });
+    });
+
+    // Navbar toggler for responsive menu
+    const navbarToggler = document.querySelector('.navbar-toggler');
+    const navbarMenu = document.querySelector('.navbar-collapse');
+
+    navbarToggler.addEventListener('click', () => {
+        navbarMenu.classList.toggle('show');
+    });
+});
+
+(function() {
+    emailjs.init("wBASQ9TpaWAJazne4");
+})();
+
+document.getElementById('contact-form').addEventListener('submit', function(event) {
+    event.preventDefault();
+    emailjs.sendForm('service_l2b833g', 'template_qyc3bcx', this)
+        .then(function() {
+            console.log('SUCCESS!');
+        }, function(error) {
+            console.log('FAILED...', error);
+        });
+});
