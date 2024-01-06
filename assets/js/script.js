0ebf0dc9965c172d22c6da229e3343798cef94a2
Federico Lappa <federicolappa@gmail.com>
2024-01-06 20:17:57 +0100

    update

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,34 +5,27 @@ document.addEventListener('DOMContentLoaded', () => {
             document.querySelector(this.getAttribute('href')).scrollIntoView({
                 behavior: 'smooth'
             });
-            closeNavbarMenu();
         });
     });
 
     const navbarToggler = document.querySelector('.navbar-toggler');
-    const navbarMenu = document.querySelector('.navbar-collapse');
+    const navbarMenu = document.querySelector('#navbarNav');
 
-    navbarToggler.addEventListener('click', () => {
+    navbarToggler.addEventListener('click', function() {
         navbarMenu.classList.toggle('show');
     });
 
-    function closeNavbarMenu() {
-        if (navbarMenu.classList.contains('show')) {
-            navbarMenu.classList.remove('show');
-        }
-    }
-});
+    (function() {
+        emailjs.init("wBASQ9TpaWAJazne4");
+    })();
 
-(function() {
-    emailjs.init("wBASQ9TpaWAJazne4");
-})();
-
-document.getElementById('contact-form').addEventListener('submit', function(event) {
-    event.preventDefault();
-    emailjs.sendForm('service_l2b833g', 'template_qyc3bcx', this)
-        .then(function() {
-            console.log('SUCCESS!');
-        }, function(error) {
-            console.log('FAILED...', error);
-        });
+    document.getElementById('contact-form').addEventListener('submit', function(event) {
+        event.preventDefault();
+        emailjs.sendForm('service_l2b833g', 'template_qyc3bcx', this)
+            .then(function() {
+                console.log('SUCCESS!');
+            }, function(error) {
+                console.log('FAILED...', error);
+            });
+    });
 });
